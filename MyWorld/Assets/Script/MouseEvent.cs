ed4e2b6a8a0a50450918f0ebc65cc0116c864dcd
ZhArom <3100038995@qq.com>
2023-06-21 17:54:35 +0800

    my world

---
+++ MyWorld/Assets/Script/MouseEvent.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class MouseEvent : MonoBehaviour
+{
+    [Header("玩家建模方块")]
+    public GameObject Wood;
+
+    void DestroyCube()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rhit;
+        if (Physics.Raycast(ray, out rhit, 7, LayerMask.GetMask("Cube")))
+        {
+            Vector3 pos = rhit.point + ray.direction.normalized * 0.1f;
+            WorldModify wd = new WorldModify();
+            wd.x = Mathf.RoundToInt(pos.x);
+            wd.y = Mathf.RoundToInt(pos.y);
+            wd.z = Mathf.RoundToInt(pos.z);
+            wd.to = 0;
+            UserClient.SendMessage(new Message("UpdateWorld", JsonConvert.SerializeObject(wd)));
+            Debug.Log("消除一个方块！");
+        }
+    }
+
+    void PutCube()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rhit;
+        if (Physics.Raycast(ray, out rhit, 7, LayerMask.GetMask("Cube")))
+        {
+            Vector3 pos = rhit.point - ray.direction.normalized * 0.1f;
+            WorldModify wd = new WorldModify();
+            wd.x = Mathf.RoundToInt(pos.x);
+            wd.y = Mathf.RoundToInt(pos.y);
+            wd.z = Mathf.RoundToInt(pos.z);
+            wd.to = 3;
+            if (wd.inside())
+            {
+                UserClient.SendMessage(new Message("UpdateWorld", JsonConvert.SerializeObject(wd)));
+            }
+            Debug.Log("创建一个方块！");
+        }
+    }
+
+    void Update()
+    {
+        if (Global.pause) return;
+        if (Input.GetMouseButtonDown(0))
+        {
+            DestroyCube();
+
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            PutCube();
+        }
+    }
+}
---
+++ MyWorldServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("MyWorldServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyWorldServer")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+
+[assembly: Guid("216b6e94-3d5b-4fe6-b951-63cda54db5e3")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
